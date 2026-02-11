04f1c7ede77bf8e3358ace883c32e38e26c2a6fa
Junaidchohane <junaidchohanedu@gmail.com>
2026-02-11 20:43:47 +0500

    Data Types in Python

---
+++ 01_variables_and_data_types/data_types.py
@@ -0,0 +1,15 @@
+# Interger
+my_age = 25
+# Float
+pi = 3.14
+# String
+name = "Muhammad Junaid"
+# Boolean
+is_student = True
+# None
+address = None
+print("My name is ", name, "and I am", my_age, "years old.")
+
+print(type(pi))
+print(type(is_student))
+print(type(address))
