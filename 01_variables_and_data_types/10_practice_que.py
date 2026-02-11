4adcf7e7486e9398f2012f3b611fa0b921f43341
Junaidchohane <junaidchohanedu@gmail.com>
2026-02-12 01:40:42 +0500

    Python practice Q

---
+++ 01_variables_and_data_types/10_practice_que.py
@@ -0,0 +1,16 @@
+# num1 = int(input("Enter number1 : "))
+# num2 = int(input("Enter number2 : "))
+# print(num1+num2)
+
+
+# side = float(input("enter square side : "))
+# print("area =", side * side)
+
+
+# num1 = float(input("Enter number1 : "))
+# num2 = float(input("Enter number2 : "))
+# print("avg =", (num1+num2)/2)
+
+num1 = int(input("Enter number1 : "))
+num2 = int(input("Enter number2 : "))
+print(num1 >= num2)
