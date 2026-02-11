d44537cdc4f4bdde96e41718a4759f7a1637c439
Junaidchohane <junaidchohanedu@gmail.com>
2026-02-11 23:56:13 +0500

    Input in Python with type conversion

---
+++ 01_variables_and_data_types/08_input_in_python.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+price = float(input("Enter the price: "))
+
+print("My name is", name, "and I am", age, "years old. The price is", price)
