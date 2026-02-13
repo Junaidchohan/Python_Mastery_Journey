a9779b20a9b0718001272188f54a1cf1963dc8d9
Junaidchohane <junaidchohanedu@gmail.com>
2026-02-13 12:07:38 +0500

    Lists in python

---
+++ 03_lists_and_tuples/02_tuples.py
@@ -0,0 +1,16 @@
+# tuples like as same as list but its immuatable, we cant re assign value to them
+
+tup = (1, 2, 3, 4, 5, 5, 5, 5, 5, 5)
+print(tup)
+print(type(tup))
+
+# Methods in tuples
+
+# 01 Index method
+tup = (1, 2, 3, 4, 5, 5, 5, 5, 5, 5)
+print(tup.index(3)) # its method show us to when first time in the tup and which one index eliment come
+
+
+# 02 Index method
+tup = (1, 2, 3, 4, 5, 5, 5, 5, 5, 5)
+print(tup.count(5)) # count the value of element
