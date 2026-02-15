82700ab73fece9b87c0ca01daf9fadd303a494c2
Junaidchohane <junaidchohanedu@gmail.com>
2026-02-15 14:04:33 +0500

    Abstraction in Python

--- 07_OOPS/01_opps.py
+++ 07_OOPS/01_opps.py
@@ -1,28 +1,28 @@
-# # Ex01
-# class Students:
-#     name = "Junaid"
+# Ex01
+class Students:
+    name = "Junaid"
 
-# s1 = Students()
-# print(s1.name)
+s1 = Students()
+print(s1.name)
 
-# s2 = Students()
-# print(s2.name)
+s2 = Students()
+print(s2.name)
 
-# # Ex02
-# class Car:
-#     color = "blue"
-#     brand = "mercedes"
+# Ex02
+class Car:
+    color = "blue"
+    brand = "mercedes"
 
-# car1 = Car()
-# print(car1.color, car1.brand)
+car1 = Car()
+print(car1.color, car1.brand)
 
 
 
 # Constractor in python
 class Students:
-    college_name = "SSPI" # Attributes of class its create one time in class, but name & marks create 2 time even as much we create the obj and as much auto create the name and marks
+    college_name = "SSPI" # class Attributes of class its create one time in class, but name & marks create 2 time even as much we create the obj and as much auto create the name and marks
     def __init__(self, name, marks):
-        self.name = name
+        self.name = name # Object Attributes
         self.marks = marks
         print("Adding new students in database")
     #  Methods in python
@@ -36,4 +36,4 @@ s2 = Students("Umair", 98)
 print(s2.name, s2.marks)
 
 print(Students.college_name)
-s2.wellcome()
+s2.wellcome() # call to method
---
+++ 08_OOPS_4_Pillers/01_Abstraction.py
@@ -0,0 +1,14 @@
+# Hiding the implementaion details of class and showing the essential features to user.
+class Car:
+    def __init__(self):
+        self.acc = False
+        self.brk = False
+        self.clutch = False
+
+    def Start(self):    # these all steps hide its in class Like "self.acc = True" and "self.clutch = True"
+        self.acc = True # Unnessesry details is hide
+        self.clutch = True
+        print("Car is Start Now")
+
+car1 = Car() # We show to user just car is start now.
+car1.Start()
