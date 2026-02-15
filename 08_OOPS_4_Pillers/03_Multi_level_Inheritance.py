55030785a17481a100a3c38da24ec0626c215fcc
Junaidchohane <junaidchohanedu@gmail.com>
2026-02-15 16:40:14 +0500

    Multi Level Inheritance in Python

---
+++ 08_OOPS_4_Pillers/03_Multi_level_Inheritance.py
@@ -0,0 +1,31 @@
+# Class One and its called parent class
+class Car:
+    @staticmethod
+    def Start():
+        print("car start..")
+
+
+    @staticmethod
+    def Stop():
+        print("car stop.")
+
+
+# Class second and its called child class and here parent class we write in ()
+class ToyotaCar(Car):
+    def __init__(self, brand):
+        self.name = brand
+
+
+# Class thired and its called child class of ToyotaCar and its inherit the all property & Method of Car & ToyotaCar
+class Fortuner(ToyotaCar):
+    def __init__(self, type):
+        self.type = type
+
+
+
+
+car1 = Fortuner("deisel")
+
+print(car1.Start) # Here we access our parent class Propertys/Attributes as (Start Method)
+print(car1.type) # # Here we access our parent class Propertys/Attributes as (color property)
+
